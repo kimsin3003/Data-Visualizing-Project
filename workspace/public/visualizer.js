6a47d0345c5dcbdd5eb2dcddbbc5124f58d476e9
taewoo <kimsin3003@naver.com>
2016-05-21 20:00:37 +0900

    temp

--- workspace/public/visualizer.js
+++ workspace/public/visualizer.js
@@ -1,32 +1,32 @@
-/*
-d3.json("./custom.geo.json", function(json){
-  var w = 1000;
-  var h = 500;
-  var projection = d3.geo.azimuthalEqualArea().translate([w/2,h/2]);
-  var path = d3.geo.path().projection(projection);
-  var svg = d3.select("body").append("svg")
-                            .attr("width", w)
-                            .attr("height", h);
-
-  svg.selectAll("path")
-  .data(json.features)
-  .enter().append("path")
-  .attr("d", path);
-});
-
-d3.csv("./apple.csv", function(error, data) {
-  dataSet = data;
-  console.log(dataSet);
-  d3.select("body").data(dataSet).enter().append("div").classed("bar", true).text(function(d){ return d.Date; }).style("height", function(d){
-    return d.Close*2 + "px";
-  });
-});
-*/
 
 d3.xhr('/data', function(XMLReq){
   dataSet = JSON.parse(XMLReq.response);
-  d3.select("body").data(dataSet).enter().append("div").classed("bar", true).text(function(d){ console.log(d.Date); return d.Date; }).style("height", function(d){
 
-    return d.Price / 10 + "px";
+  var svg = d3.select("body").append("svg").attr("width", 1000).attr("height", 1000);
+
+  svg.selectAll("rect")
+    .data(dataSet)
+    .enter()
+    .append("rect")
+    .attr("x", function(d, i){
+      return i *  20;
+    })
+    .attr("y", function(d, i){
+      return d.Price;
+    })
+    .attr("width", 20)
+    .attr("height", 100);
+  /*
+  var circle = svg.selectAll("circle").data(dataSet).enter().append("circle");
+  circle.attr("cx", function(d,i){
+      return (i * 30);
+  }).attr("cy", function(d,i){
+      return (50);
+  }).attr("r", function(d,i){
+      return (d.Price / 50);
+  }).attr("fill", "yellow").attr("stroke", "orange").attr("stroke-width", function(d){
+    return d.Price / 150;
   });
+  */
+
 });
